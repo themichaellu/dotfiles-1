e183142dd6653c9389bf39dbd120d326f114ca70
Ferenc Szalai <szferi@pionlab.com>
2013-05-15 01:20:11 +0200

    add nerd tree

--- update.sh
+++ update.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env zsh
+# bootstrap zsh
 if [[ ! -d $HOME/antigen ]]; then
 	git clone https://github.com/zsh-users/antigen $HOME/antigen
 fi
 cp zsh/zshrc $HOME/.zshrc
+
+# bootstrap vim
+if [[ ! -d $HOME/.vim/bundle/vundle ]]; then
+	mv $HOME/.vim $HOME/.vim.old
+	rm -rf $HOME/.vim/
+	mkdir -p $HOME/.vim/bundle
+	git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+fi
+cp vim/vimrc $HOME/.vimrc
+
+# Install plugins
+vim +BundleInstall +qall
