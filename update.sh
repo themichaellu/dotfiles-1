345830b9e66a79a511191e7613b3c4347e51e399
Ferenc Szalai <szferi@pionlab.com>
2013-05-15 04:14:54 +0200

    add powerline config

--- update.sh
+++ update.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env zsh
 # bootstrap zsh
-if [[ ! -d $HOME/antigen ]]; then
+if [ ! -d $HOME/antigen ]; then
 	git clone https://github.com/zsh-users/antigen $HOME/antigen
 fi
 cp zsh/zshrc $HOME/.zshrc
 
 # bootstrap vim
-if [[ ! -d $HOME/.vim/bundle/vundle ]]; then
+if [ ! -d $HOME/.vim/bundle/vundle ]; then
 	mv $HOME/.vim $HOME/.vim.old
 	rm -rf $HOME/.vim/
 	mkdir -p $HOME/.vim/bundle
 	git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 fi
 cp vim/vimrc $HOME/.vimrc
+
+# configuring vim/powerline fonts
+if [ ! -f $HOME/.fonts/PowerlineSymbols.otf ]; then
+	mkdir -p $HOME/.fonts
+       	cp fonts/PowerlineSymbols.otf $HOME/.fonts/
+	fc-cache -vf $HOME/.fonts/
+fi
+
+if [ ! -f $HOME/.fonts.conf.d/10-powerline-symbols.conf ]; then
+	mkdir -p $HOME/.fonts.conf.d/
+	cp fonts/10-powerline-symbols.conf $HOME/.fonts.conf.d/
+fi
 
 # Install plugins
 vim +BundleInstall +qall
